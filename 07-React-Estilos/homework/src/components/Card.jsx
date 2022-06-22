ac98fa8a9624d5ddadb464affff836d2d83f32dd
AlexT-Dev <atacolmx@gmail.com>
2022-06-22 15:30:09 -0500

    Tarea de React estilos

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import s from './Card.module.css';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div>
+      <div className={s.columElements}>
+          <div className={s.container}>
+            <button className={s.btnClose} onClick={props.onClose} >X</button>
+            <h4 className={s.tituloCard}>{props.name}</h4>
+            <div className={s.grid}>
+              <div className={s.divData}>
+                <p className={s.colorData}>Min</p>  
+                <p>{props.min}</p>
+              </div>
+              <div className={s.divData}>
+                <p className={s.colorData}>Max</p>
+                <p>{props.max}</p>
+              </div>
+              <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="imagen no encontrada" />
+            </div>
+          </div>
+        </div>
+        </div>    
+  )
 };
